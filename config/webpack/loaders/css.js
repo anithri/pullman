0a8cde17a7718dc1780e93f3187dd7d94ebe6fb4
Scott M Parrish <anithri@gmail.com>
2017-07-02 11:34:49 -0600

    clean out sass stuff

---
+++ config/webpack/loaders/css.js
@@ -0,0 +1,23 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const {env} = require('../configuration.js')
+
+module.exports = {
+  test: /\.css$/i,
+  use: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: [
+      {
+        loader: 'css-loader',
+        options: {
+          minimize: env.NODE_ENV === 'production',
+          modules: true,
+          sourceMap: true,
+          importLoaders: 2,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
+      },
+      {loader: 'postcss-loader', options: {sourceMap: true}},
+      'resolve-url-loader'
+    ]
+  })
+}
