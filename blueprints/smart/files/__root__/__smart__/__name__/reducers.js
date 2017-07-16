6db4075aa7eda0eb23e07b78c1a6d01bbe575f57
Scott M Parrish <anithri@gmail.com>
2017-07-15 19:28:11 -0600

    refactor smart blueprint to create an entire store slice

---
+++ blueprints/smart/files/__root__/__smart__/__name__/reducers.js
@@ -0,0 +1,18 @@
+// Constants
+// const SOMETHING_CHANGED = 'cards/<%= camelEntityName %>/something_changed'
+export const constants = {
+}
+// Action Creators
+export const actions = {
+}
+
+// Reducer
+export const defaultState = {
+}
+
+export default function (state = defaultState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
