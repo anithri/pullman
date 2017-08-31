76f01754534556303d54694bd05f026eb6fd253b
Scott M Parrish <anithri@gmail.com>
2017-08-30 19:40:00 -0600

    more work on card views;

---
+++ app/javascript/screens/Card/Detail/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import cx from 'classnames'
+
+import styles from '../styles.css'
+
+class Summary extends React.Component {
+  render () {
+    const myClasses = cx(
+      this.props.className,
+      styles.card,
+      styles.summary,
+      this.props.cardClass
+    )
+    return (
+      <div className={myClasses}>
+        <header>
+          <h3>{this.props.name}</h3>
+        </header>
+      </div>
+    )
+  }
+}
+Summary.propTypes = {}
+export default Summary
